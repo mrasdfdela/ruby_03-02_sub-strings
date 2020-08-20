2517dc4fc4a2b1740afd5e13e8416478297ef927
mrasdfdela <jerryhsu830@gmail.com>
2020-08-20 18:50:34 -0400

    substrings exercise

--- sub-strings.rb
+++ sub-strings.rb
@@ -1,1 +1,19 @@
-puts "hello world!"
+require "byebug"
+
+def substrings(string, dictionary)
+    str = {}
+
+    dictionary.each do |word|
+        if string.include?(word)
+            hash = "#{word}"
+            # byebug
+            str[hash] ? str[hash] += 1 : str[hash] = 1
+        end
+    end
+
+    return str
+end
+
+dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
+answer = { 'below' => 1, 'low' => 1 }
+puts "test: #{ substrings('below', dictionary) == answer }"
