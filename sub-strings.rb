f13f71bbe6d09f3d6c1ddaa34c0cbff306e8dd8a
mrasdfdela <jerryhsu830@gmail.com>
2020-08-20 21:19:13 -0400

    substrings solution, updated

--- sub-strings.rb
+++ sub-strings.rb
@@ -2,12 +2,13 @@ require "byebug"
 
 def substrings(string, dictionary)
     str = {}
+    split_string = string.downcase.split(" ")
 
-    dictionary.each do |word|
-        if string.include?(word)
-            hash = "#{word}"
-            # byebug
-            str[hash] ? str[hash] += 1 : str[hash] = 1
+    split_string.each do |split_word|
+        dictionary.each do |word|
+            if split_word.include?(word)
+                str[word] ? str[word] += 1 : str[word] = 1
+            end
         end
     end
 
@@ -16,4 +17,7 @@ end
 
 dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
 answer = { 'below' => 1, 'low' => 1 }
-puts "test: #{ substrings('below', dictionary) == answer }"
+puts "test 1: #{ substrings('below', dictionary) == answer }"
+
+answer_2 = { "down" => 1, "go" => 1, "going" => 1, "how" => 2, "howdy" => 1, "it" => 2, "i" => 3, "own" => 1, "part" => 1, "partner" => 1, "sit" => 1 }
+puts "test 2: #{ substrings("Howdy partner, sit down! How's it going?", dictionary) == answer_2 } "
